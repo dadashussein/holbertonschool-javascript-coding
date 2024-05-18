ce6823ad1eb9c77a8cdb62a0b52689ef86c1ca97
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-05-18 14:02:51 +0400

    fix json parse

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -2,8 +2,8 @@
 const request = require('request');
 request(`https://swapi-api.hbtn.io/api/films/${process.argv[2]}`, (error, response) => {
   if (error) {
-    console.log(error);
+    console.error(error);
     return;
   }
-  console.log(response.body.title);
+  console.log(JSON.parse(response.body).title);
 });
